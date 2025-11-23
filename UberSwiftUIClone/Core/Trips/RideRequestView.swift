fd953d8fab7ee2fe97b920d934bd0eb5b7bf5a2d
Ahmed Mohamed Fouad <63275575+ahmedfouad01099@users.noreply.github.com>
2025-11-23 21:18:47 +0200

    Finish the design of Ride Request UI View

---
+++ UberSwiftUIClone/Core/Trips/RideRequestView.swift
@@ -0,0 +1,159 @@
+//
+//  RideRequestView.swift
+//  UberSwiftUIClone
+//
+//  Created by Ahmed Fouad on 23/11/2025.
+//
+
+import SwiftUI
+
+struct RideRequestView: View {
+    var body: some View {
+        VStack {
+            Capsule()
+                .foregroundColor(Color(.systemGray5))
+                .frame(width: 48, height: 6)
+
+            HStack {
+                VStack {
+                    Circle()
+                        .fill(Color(.systemGray3))
+                        .frame(width: 8, height: 8)
+
+                    Rectangle()
+                        .fill()
+                        .frame(width: 1, height: 24)
+
+                    Circle()
+                        .fill(.black)
+                        .frame(width: 8, height: 8)
+                }
+                .padding(.horizontal)
+
+                VStack(alignment: .leading) {
+                    Text("Current Loaction")
+                        .font(.headline)
+                        .foregroundColor(Color(.systemGray2))
+                        .padding(.bottom)
+
+                    Text("Mansura, Egypt")
+                        .font(.headline)
+                        .foregroundColor(Color(.black))
+                }
+
+                Spacer()
+
+                VStack(alignment: .leading) {
+                    Text("02:55 PM")
+                        .font(.headline)
+                        .foregroundColor(Color(.systemGray3))
+                        .padding(.bottom)
+
+                    Text("03:11 PM")
+                        .font(.headline)
+                        .foregroundColor(Color(.systemGray3))
+                }
+                .padding()
+
+            }
+            .padding(.top, 10)
+
+            Divider()
+
+            VStack(alignment: .leading) {
+                Text("SUGGESTED RIDES")
+                    .font(.headline)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .foregroundColor(Color(.systemGray))
+
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 16) {
+                        ForEach(0..<5, id: \.self) { index in
+                            Rectangle()
+                                .fill(Color.gray.opacity(0.2))
+                                .frame(width: 120, height: 180)
+                                .cornerRadius(8)
+                                .overlay(
+                                    VStack(spacing: 0) {
+                                        Image("uber-white")
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fit)
+                                            .frame(width: 180, height: 120)
+
+                                        VStack(alignment: .leading, spacing: 4)
+                                        {
+                                            Text("UberX")
+                                                .font(.headline)
+                                                .foregroundColor(.black)
+                                            Text("$22.04")
+                                                .font(.headline)
+                                                .foregroundColor(.gray)
+                                        }
+                                        .frame(
+                                            maxWidth: .infinity,
+                                            alignment: .leading
+                                        )
+                                        .padding(.horizontal)
+                                        .padding(.bottom, 8)  // Add bottom padding only
+                                    }
+                                )
+                        }
+                    }
+                }
+
+            }
+            .padding()
+
+            Button {
+
+            } label: {
+                HStack {
+                    Rectangle()
+                        .fill(Color.blue)
+                        .frame(width: 60, height: 40)
+                        .cornerRadius(8)
+                        .overlay {
+                            Text("Visa")
+                                .foregroundColor(Color.white)
+                                .font(.headline)
+                        }
+
+                    Text("*** 1234")
+                        .font(.headline)
+                        .foregroundColor(Color(.systemGray))
+
+                    Spacer()
+
+                    Image(systemName: "chevron.right")
+                        .font(.system(size: 20, weight: .medium))
+                        .foregroundColor(.black)
+                }
+                .padding(10)
+            }
+            .background(Color.gray.opacity(0.2))
+            .cornerRadius(8)
+            .padding()
+
+            Button {
+
+            } label: {
+                VStack {
+                    Text("CONFIRM RIDE")
+                        .foregroundColor(.white)
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                }
+                .frame(maxWidth: .infinity, alignment: .center)
+                .padding(14)
+            }
+            .background(Color.blue.opacity(0.8))
+            .cornerRadius(8)
+            .padding()
+        }
+        .background(.white)
+    }
+}
+
+#Preview {
+    RideRequestView()
+}
